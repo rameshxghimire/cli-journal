5c9634bd15b61eb0367f215713683c4f4f4ec7cc
Ramesh Ghimire <rameshxghimire@gmail.com>
2018-12-17 18:12:38 +0200

    edited codes, modified some docstrings and added menu code.

--- journal.py
+++ journal.py
@@ -1,8 +1,7 @@
 # Importing datetime system module for recording the entry dates.
 import datetime
 """
-journal.py - This module will contain the Journal object with a list of journals and individual entries (or daily journals)
-as another object.
+journal module - Contains Journal object with a list of journals and entrie.
 """
 
 version = "1.0.0"
@@ -11,12 +10,11 @@ last_id = 0        # Initializing a variable to store journal ids.
 
 
 class Entry:
-    """ Represents daily journal entries in the Journal (A Journal is a collection of such entries). 
-    The entries in the journal have the content and tags to identify them.
+    """ Represents daily journal entries in the Journal.
     """
 
     def __init__(self, content, tags=""):
-        """Initializes the entries with content and tag field, assigns date and id automatically."""
+        """Initializes the entries with content and tag field."""
         global last_id
         self.tags = tags
         self.written_date = datetime.date.today()
@@ -26,9 +24,9 @@ class Entry:
         self.id = last_id
 
     def match(self, searchtext):
-        """ Finding out if the note is retrived using a search text, this is important for the search functionality.
-        Seach can also be performed on tags.
+        """ Finds out if the note is retrived using a search text.
         """
+        # Ignore the character case
         return searchtext in self.content and self.tags
 
     def setContent(self, content):
@@ -39,7 +37,7 @@ class Entry:
 
 
 class Journal:
-    """ Journal represents a collection of Entries, it is like a folder where files are like Entries which are kept here.
+    """ Represents a collection of Entries.
     """
 
     def __init__(self):
@@ -47,13 +45,9 @@ class Journal:
         self.entries = []
 
     def new_entry(self, content, tags=""):
-       # Create a new Entry and append it in the list.
+        # Create a new Entry and append it in the list.
         self.entries.append(Entry(content, tags))
 
-    # edit entry of given id
-    # @param entry_id [Integer] entry id
-    # @param content [String] new content to update/edit
-    # @return void sets entry
     def edit_entry(self, entry_id, content):
         """ Change the content of an entry by finding it with id."""
         for instance in self.entries:
--- menu.py
+++ menu.py
@@ -3,10 +3,10 @@ This is an interface for the user to use the journal. It provides users options 
 """
 import sys
 
-from journal import Entries, Journal
+from journal import Entry, Journal
 
 
-class menu:
+class Menu:
     """ Show menu options for the user."""
 
     def __init__(self):
@@ -16,14 +16,52 @@ class menu:
             "2": self.search_entries,
             "3": self.add_entries,
             "4": self.edit_entries,
-            "5": self.close
+            "5": self.quit
         }
 
     def show_menu(self):
-        print("""
-        1. Show all entries.
-        2. Search entries.
-        3. Add a new entry.
-        4. Edit an entry.
-        5. Close the journal.
-        """)
+        print("1. Show all entries.\n2. Search entries.\3. Add a new entry.\n4. Edit an entry.\n5. Close the journal.\n")
+
+    def run(self):
+        '''Display the menu and respond to choices.'''
+        while True:
+            self.show_menu()
+            choice = input("Enter an option: ")
+            action = self.options.get(choice)
+            if action:
+                action()
+            else:
+                print(f"{choice} is not a valid choice. Try again!")
+
+    def show_entries(self, entries=None):
+        if not entries:
+            entries = self.journal.entries
+        for entry in entries:
+            print(f"{entry.id}: {entry.tags}\n{entry.content}")
+
+    def search_entries(self):
+        filter = input("Search for: ")
+        content = self.journal.search(filter)
+        self.show_entries(entries)
+
+    def add_entries(self):
+        content = input("Enter an entry: ")
+        self.journal.new_entry(content)
+        print("Your journal entry has been added.")
+
+    def edit_entries(self):
+        id = input("Enter an id: ")
+        content = input("Enter the journal entry: ")
+        tags = input("Enter tags: ")
+        if content:
+            self.journal.modify_entries(id, content)
+        if tags:
+            self.journal.modify_tags(id, tags)
+
+    def quit(self):
+        print("Thank you for using your journal today.")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    Menu().run()
+
